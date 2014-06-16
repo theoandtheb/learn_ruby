ad9b39d1ba4e15832efa0680b817303bc20ba10e
Jon <jonssmall@gmail.com>
2014-06-16 16:43:42 -0400

    Finished 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,18 +6,37 @@ def shout(x)
 	return "#{x.upcase}"
 end
 	
-def repeat(x,y)
-	if y == nil
-		array << "#{x} #{x}"
-	else
-		(y-1).times do
-			array << "#{x} "
-		end
+def repeat(x,y=2)
+	array = []
+	k = String.new
+	(y-1).times do
 		array << "#{x}"
 	end
-	return array.to_s
+		array << "#{x}"
+	k = array.join(" ")
+	return "#{k}"
 end
 
 def start_of_word(x,y)
-	return x[1..y]
+	return x[0..(y-1)]
 end
+
+def first_word(x)
+	return x.split[0]
+end
+
+
+def titleize(x)
+	startArray = x.split(' ')
+	capArray = []
+	exemptArray = ["and", "or", "the", "over", "to", "the", "a", "but"]
+	startArray.each do |x|
+		if exemptArray.include?(x)
+			capArray << x
+		else
+			capArray << x.capitalize
+		end
+	end
+	capArray[0] = capArray[0].capitalize
+	return capArray.join(' ')
+end 
